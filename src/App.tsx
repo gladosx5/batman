5cb747b71d18473f45303347d4bfed5821843ec2
gladosx3 <gladosx3.1@gmail.com>
2025-09-16 12:32:10 +0200

    Séparer complètement Gotham et le site web

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import GothamScene from './components/GothamScene';
+import GothamPortal from './components/GothamPortal';
 import FoodWebsite from './components/FoodWebsite';
-import Header from './components/Header';
 import { useScrollAnimation } from './hooks/useScrollAnimation';
 import './styles/animations.css';
 import './styles/food-website.css';
@@ -12,10 +11,10 @@ function App() {
 
   return (
     <div className="app">
-      {/* Site web Gotham Streat */}
+      {/* Site web principal */}
       <FoodWebsite />
-      {/* Scène Gotham avec animation Batman par-dessus */}
-      <GothamScene />
+      {/* Scène Gotham dans un portail séparé */}
+      <GothamPortal />
     </div>
   );
 }
